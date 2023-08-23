b8e0bbed7b13e29a51e897b73d081981fabac7b5
Bruno Sousa <brunolpsousa@gmail.com>
2023-08-23 17:07:01 -0300

    refactor: remove day/time from instructor model

--- src/models/instructorModel.ts
+++ src/models/instructorModel.ts
@@ -34,9 +34,7 @@ const InstructorSchema = new Schema(
         busy: {
           type: Boolean,
           default: false
-        },
-        day: String,
-        time: String
+        }
       }
     ]
   },
@@ -48,22 +46,6 @@ InstructorSchema.virtual('classes', {
   localField: '_id',
   foreignField: 'instructor',
   justOne: false
-})
-
-// Perhaps this should be moved to a controller
-// This was supposed to make it easier to match queries by day or time,
-// but maybe it's not necessary to store it in DB
-InstructorSchema.pre('save', function (next) {
-  try {
-    this.schedule.forEach(e => {
-      e.day = e.date!.toISOString().split('T')[0]
-      e.time = e.date!.toLocaleTimeString('pt-BR', { timeStyle: 'short' })
-    })
-  } catch (error) {
-    console.log(error)
-  } finally {
-    next()
-  }
 })
 
 InstructorSchema.pre(/delete/i, async function () {
