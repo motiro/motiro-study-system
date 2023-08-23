66b56775c955561c2ad62ea31f5e6685d189ed3b
Bruno Sousa <brunolpsousa@gmail.com>
2023-08-23 11:41:28 -0300

    feat: add pre-save hook into instructor model

--- src/models/instructorModel.ts
+++ src/models/instructorModel.ts
@@ -43,6 +43,19 @@ InstructorSchema.virtual('classes', {
   justOne: false
 })
 
+// Perhaps this should be moved to a controller
+// This was supposed to make it easier to match queries by day or time,
+// but maybe it's not necessary to store it in DB
+InstructorSchema.pre('save', function (next) {
+  try {
+    this.schedule.forEach(e => {
+      e.day = e.date!.toISOString().split('T')[0]
+      e.time = e.date!.toLocaleTimeString('pt-BR', { timeStyle: 'short' })
+    })
+  } catch (error) {
+    console.log(error)
+  } finally {
+    next()
   }
 })
 
