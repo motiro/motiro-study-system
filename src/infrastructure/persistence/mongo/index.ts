9847e3b075d2a647dfd2afe4d23740934483cc91
Bruno Sousa <brunolpsousa@gmail.com>
2023-08-28 13:02:01 -0300

    refactor: add mongo's index.ts

---
+++ src/infrastructure/persistence/mongo/index.ts
@@ -0,0 +1,5 @@
+import { instructorModel } from './instructorModel'
+import { studentModel } from './studentModel'
+import { lessonModel } from './lessonModel'
+
+export { instructorModel, studentModel, lessonModel }
