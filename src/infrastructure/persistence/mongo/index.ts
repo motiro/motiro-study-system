c1fc9d93e408f8398299dda5fd54ab33f1288bf0
Bruno Sousa <brunolpsousa@gmail.com>
2023-08-28 13:03:07 -0300

    feat: add user schema interface

--- src/infrastructure/persistence/mongo/index.ts
+++ src/infrastructure/persistence/mongo/index.ts
@@ -1,5 +1,10 @@
+import { Schema } from 'mongoose'
 import { instructorModel } from './instructorModel'
 import { studentModel } from './studentModel'
 import { lessonModel } from './lessonModel'
+
+export interface UserSchema extends Schema {
+  comparePassword(password: string): boolean
+}
 
 export { instructorModel, studentModel, lessonModel }
