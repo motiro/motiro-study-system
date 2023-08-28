4eee60acb9e72dc1a660421972e564fb30b79b58
Bruno Sousa <brunolpsousa@gmail.com>
2023-08-28 15:18:50 -0300

    feat: use cookie parser

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import express from 'express'
+import cookieParser from 'cookie-parser'
 import 'dotenv/config'
 
 const app = express()
 
-const port = process.env.PORT || 5000
+const { PORT, COOKIE_SECRET } = process.env
+const port = PORT || 5000
 
 app.use(express.json())
+app.use(cookieParser(COOKIE_SECRET))
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`)
