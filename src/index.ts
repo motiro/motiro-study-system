3749e58e851d1c47fa504542aebeb8048ac31400
Karen Passarella <karenpassarella@hotmail.com>
2023-08-28 13:03:18 -0300

    docs: Adicionada rota para documentação

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import express from 'express'
 import 'dotenv/config'
+import swaggerUi from "swagger-ui-express"
+import swaggerDocs from "./swagger.json"
 
 const app = express()
 
 const port = process.env.PORT || 5000
 
 app.use(express.json())
+
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`)
