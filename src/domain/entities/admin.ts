5a89c56e21cf40bcc4ef803041839ab472f7f8f9
Bruno Sousa <brunolpsousa@gmail.com>
2023-08-29 20:01:54 -0300

    feat: add admin entity

---
+++ src/domain/entities/admin.ts
@@ -0,0 +1,24 @@
+export interface showAdmin {
+  name: string
+  email: string
+  password?: string
+}
+
+export class Admin {
+  public readonly id?: string
+  public name: string
+  public email: string
+  public password: string
+  public role?: string
+
+  constructor(props: Omit<Admin, 'id'>, id?: string) {
+    this.name = props.name
+    this.email = props.email
+    this.password = props.password
+    this.role = 'admin'
+
+    if (id) {
+      this.id = id
+    }
+  }
+}
