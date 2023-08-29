6603536e98f2f45c8effbfe8e69126382c975601
Bruno Sousa <brunolpsousa@gmail.com>
2023-08-29 20:38:57 -0300

    feat: use Schema<Admin> in adminRepository

--- src/domain/repository/adminRepository.ts
+++ src/domain/repository/adminRepository.ts
@@ -2,8 +2,8 @@ import { Admin, showAdmin } from 'domain/entities/admin'
 import { Schema } from 'mongoose'
 
 export interface AdminRepository {
-  findAdminById(id: string): Promise<Schema | null>
-  findAllAdmins(): Promise<Schema[]>
+  findAdminById(id: string): Promise<Schema<Admin> | null>
+  findAllAdmins(): Promise<Schema<Admin>[]>
   saveAdmin(instructor: Admin): Promise<showAdmin>
   updateAdmin(instructor: Admin): Promise<void>
   deleteAdmin(id: string): Promise<void>
