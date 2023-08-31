0b51b6df8d55dc00c3cd53c4d891749e872b60ea
Bruno Sousa <brunolpsousa@gmail.com>
2023-08-31 15:36:50 -0300

    fix: cookie parser placed below the express router

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,16 @@
 import express from 'express'
 import router from './routes'
 import cookieParser from 'cookie-parser'
-import 'dotenv/config'
 import './infrastructure/persistence/mongo/connections'
-
-const app = express()
+import 'dotenv/config'
 
 const { PORT, COOKIE_SECRET } = process.env
 const port = PORT || 5000
-
-app.use(express.json())
-app.use(router)
+const app = express()
 
 app.use(cookieParser(COOKIE_SECRET))
+app.use(express.json())
+app.use(router)
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`)
