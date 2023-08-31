c0f3da5552dcdb82f5f198dd8cbb8cf50976f94b
valeriabarci <vahc.201@gmail.com>
2023-08-31 10:18:05 -0300

    feat: add jwt protection to routes

--- src/controllers/studentsController.ts
+++ src/controllers/studentsController.ts
@@ -1,70 +1,74 @@
-import { Request, Response } from 'express';
-import { studentModel } from '@mongo/studentModel';
+import { Request, Response } from 'express'
+import { studentModel } from '@mongo/studentModel'
+import { authMiddleware } from '../applications/middlewares/authMiddleware.ts'
 
 class StudentController {
   async create(req: Request, res: Response) {
     try {
-      const { name, email, password } = req.body;
-      const student = new studentModel({ name, email, password });
-      await student.save();
+      const { name, email, password } = req.body
+      const student = new studentModel({ name, email, password })
+      await student.save()
       return res.status(201).json(student);
     } catch (error) {
-      return res.status(500).json({ error: 'Failed to create student' });
+      return res.status(500).json({ error: 'Failed to create student' })
     }
   }
 
   async update(req: Request, res: Response) {
     try {
-      const { id } = req.params;
-      const { name, email, password } = req.body;
+      const { id } = req.params
+      const { name, email, password } = req.body
       const student = await studentModel.findByIdAndUpdate(
         id,
         { name, email, password },
         { new: true }
       );
       if (!student) {
-        return res.status(404).json({ error: 'Student not found' });
+        return res.status(404).json({ error: 'Student not found' })
       }
-      return res.json(student);
+      return res.json(student)
     } catch (error) {
-      return res.status(500).json({ error: 'Failed to update student' });
+      return res.status(500).json({ error: 'Failed to update student' })
     }
   }
 
   async list(req: Request, res: Response) {
     try {
-      const students = await studentModel.find();
-      return res.json(students);
+      const students = await studentModel.find()
+      return res.json(students)
     } catch (error) {
-      return res.status(500).json({ error: 'Failed to fetch students' });
+      return res.status(500).json({ error: 'Failed to fetch students' })
     }
   }
 
   async listStudent(req: Request, res: Response) {
     try {
       const { id } = req.params;
-      const student = await studentModel.findById(id);
+      const student = await studentModel.findById(id)
       if (!student) {
-        return res.status(404).json({ error: 'Student not found' });
+        return res.status(404).json({ error: 'Student not found' })
       }
-      return res.json(student);
+
+      authMiddleware.checkUserPermissions(req.body.user, student._id)
+
+      return res.json(student)
     } catch (error) {
-      return res.status(500).json({ error: 'Failed to fetch student' });
+      return res.status(500).json({ error: 'Failed to fetch student' })
     }
   }
 
   async delete(req: Request, res: Response) {
     try {
-      const { id } = req.params;
-      const deletedStudent = await studentModel.findByIdAndDelete(id);
+      const { id } = req.params
+      const deletedStudent = await studentModel.findByIdAndDelete(id)
       if (!deletedStudent) {
-        return res.status(404).json({ error: 'Student not found' });
+        return res.status(404).json({ error: 'Student not found' })
       }
-      return res.json(deletedStudent);
+      return res.json(deletedStudent)
     } catch (error) {
-      return res.status(500).json({ error: 'Failed to delete student' });
+      return res.status(500).json({ error: 'Failed to delete student' })
     }
   }
 }
 
-export default new StudentController();
+export default new StudentController()
