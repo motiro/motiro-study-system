e5a9023b7a452a5d77f95cafcf284f2b659ba4b7
Bruno Sousa <brunolpsousa@gmail.com>
2023-09-04 11:59:14 -0300

    refactor: remove arrow function from register()

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -3,7 +3,7 @@ import { authController } from 'applications/controllers'
 
 const router = Router()
 
-router.post('/auth/register', (req, res) => authController.register(req, res))
+router.post('/auth/register', authController.register)
 router.post('/auth/login', authController.login)
 router.get('/auth/logout', authController.logout)
 
