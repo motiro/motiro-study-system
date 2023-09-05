ea9931e12eba3514e3be6c9236453bf961d746f2
Bruno Sousa <brunolpsousa@gmail.com>
2023-09-04 21:25:24 -0300

    refactor: move "student" from index into studentRoute

--- src/routes/studentRoute.ts
+++ src/routes/studentRoute.ts
@@ -5,19 +5,19 @@ import { authMiddleware } from 'applications/middlewares/authMiddleware'
 const router = Router()
 
 router.post(
-  '/',
+  '/student',
   authMiddleware.authUser,
   authMiddleware.checkRole('admin'),
   studentController.create
 )
 router.patch('/:id', authMiddleware.authUser, studentController.update)
 router.get(
-  '/',
+  '/student',
   authMiddleware.authUser,
   authMiddleware.checkRole('admin', 'instructor'),
   studentController.listAll
 )
-router.get('/:id', authMiddleware.authUser, studentController.listOne)
-router.delete('/:id', authMiddleware.authUser, studentController.delete)
+router.get('/student/:id', authMiddleware.authUser, studentController.listOne)
+router.delete('/student/:id', authMiddleware.authUser, studentController.delete)
 
 export default router
