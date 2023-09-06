00fae9a54a08c9369984d8c0da2e4e81be7500ce
Bruno Sousa <brunolpsousa@gmail.com>
2023-09-06 13:33:22 -0300

    refactor: instance authController in authRoute

--- src/routes/authRoute.ts
+++ src/routes/authRoute.ts
@@ -1,6 +1,31 @@
 import { Router } from 'express'
-import { authController } from 'applications/controllers'
+import { AuthController } from 'applications/controllers'
+import { AuthUseCase } from 'applications/usecases'
+import {
+  AdminUseCase,
+  InstructorUseCase,
+  StudentUseCase
+} from 'applications/usecases'
 
+import { MongoAdminRepository } from '@mongo/mongoAdminRepository'
+const mongoAdmRepository = new MongoAdminRepository()
+const adminUseCase = new AdminUseCase(mongoAdmRepository)
+
+import { MongoInstructorRepository } from '@mongo/mongoInstructorRepository'
+const mongoInstRepository = new MongoInstructorRepository()
+const instructorUseCase = new InstructorUseCase(mongoInstRepository)
+
+import { MongoStudentRepository } from '@mongo/mongoStudentRepository'
+const mongoStuRepository = new MongoStudentRepository()
+const studentUseCase = new StudentUseCase(mongoStuRepository)
+
+const authUseCase = new AuthUseCase(
+  adminUseCase,
+  instructorUseCase,
+  studentUseCase
+)
+
+const authController = new AuthController(authUseCase)
 const router = Router()
 
 router.post('/auth/register', authController.register)
