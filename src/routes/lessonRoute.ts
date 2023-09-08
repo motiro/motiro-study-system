d91aeae174ab2b12b56666eee872d06a271a7e7d
Bruno Sousa <brunolpsousa@gmail.com>
2023-09-08 20:57:59 -0300

    feat: add lesson routes

---
+++ src/routes/lessonRoute.ts
@@ -0,0 +1,28 @@
+import { Router } from 'express'
+import { MongoLessonRepository } from '@mongo/mongoLessonRepository'
+import { LessonUseCase } from 'applications/usecases'
+import { LessonController } from 'applications/controllers'
+import { authMiddleware } from 'applications/middlewares'
+
+const mongoRepository = new MongoLessonRepository()
+const lessonUseCase = new LessonUseCase(mongoRepository)
+const lessonController = new LessonController(lessonUseCase)
+const router = Router()
+
+router
+  .route('/lesson')
+  .all(authMiddleware.authUser)
+  .get(lessonController.listAll) // All lessons from all instructors?
+router
+  .route('/lesson')
+  .all(authMiddleware.authUser)
+  .post(lessonController.create)
+router
+  .route('/lesson/:id')
+  .all(authMiddleware.authUser)
+  .get(lessonController.listOne)
+  .get(lessonController.listAll) // Perhaps verify if id exists in params and return all lessons from a specific instructor?
+  .patch(lessonController.update)
+  .delete(lessonController.delete)
+
+export default router
