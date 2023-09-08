c69f5936b598291becb462a7b2f5e3bdc1459aa3
Bruno Sousa <brunolpsousa@gmail.com>
2023-09-08 20:55:06 -0300

    feat: add lesson interface

---
+++ src/domain/repositories/lessonRepository.ts
@@ -0,0 +1,10 @@
+import { Lesson } from 'domain/entities/lessons'
+
+export interface LessonRepository {
+  findById(id: string): Promise<Lesson | null>
+  findAll(): Promise<Lesson[]>
+  save(lesson: Lesson): Promise<Lesson>
+  update(lesson: Lesson): Promise<void>
+  delete(id: string): Promise<void>
+  counts(): Promise<number>
+}
