ec7055e28faf6e5656509d9d96b3b866592bc530
Karen Passarella <karenpassarella@hotmail.com>
2023-09-12 17:49:17 -0300

    feat: added folder for upload files

--- src/applications/usecases/uploadUseCase.ts
+++ src/applications/usecases/uploadUseCase.ts
@@ -3,7 +3,7 @@ import { UploadedFile } from 'express-fileupload'
 export const uploadUseCase = {
   processFile: (file: UploadedFile) => {
     return new Promise((resolve, reject) => {
-      file.mv('', function(err) {
+      file.mv('../../../public', function(err) {
         if (err) {
           reject(err)
         } else {
